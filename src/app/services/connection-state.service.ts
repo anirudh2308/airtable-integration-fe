731d006ecdedcb7c33666a31ebaf1413a322181c
Aniruddha <anirudh2308@gmail.com>
2025-10-13 22:44:37 -0400

    Persisting auth state

--- src/app/services/connection-state.service.ts
+++ src/app/services/connection-state.service.ts
@@ -3,14 +3,24 @@ import { BehaviorSubject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class ConnectionStateService {
-  private connectedSource = new BehaviorSubject<boolean>(false);
+  private connectedSource = new BehaviorSubject<boolean>(this.loadState());
   connected$ = this.connectedSource.asObservable();
+
+  private loadState(): boolean {
+    return localStorage.getItem('airtableConnected') === 'true';
+  }
 
   setConnected(value: boolean) {
     this.connectedSource.next(value);
+    localStorage.setItem('airtableConnected', String(value));
   }
 
   isConnected(): boolean {
     return this.connectedSource.value;
+  }
+
+  clearConnection() {
+    this.connectedSource.next(false);
+    localStorage.removeItem('airtableConnected');
   }
 }
